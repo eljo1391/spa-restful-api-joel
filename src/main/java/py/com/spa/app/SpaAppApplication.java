5990973791a22e7c89a08e370b666d8dbaa594af
eljo1391 <57019471+eljo1391@users.noreply.github.com>
2020-10-12 03:20:06 -0300

    Update SpaAppApplication.java

--- src/main/java/py/com/spa/app/SpaAppApplication.java
+++ src/main/java/py/com/spa/app/SpaAppApplication.java
@@ -15,24 +15,25 @@ import org.springframework.web.bind.annotation.RestController;
 
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.context.annotation.Bean;
 
 
 @SpringBootApplication@Configuration
-@Configuration
 public class SpaAppApplication implements WebMvcConfigurer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpaAppApplication.class, args);
 	}
 	
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-
-        registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD")
-                .allowCredentials(true)
-        ;
-    }
+	   @Bean
+	   public WebMvcConfigurer corsConfigurer() {
+	      return new WebMvcConfigurerAdapter() {
+	         @Override
+	         public void addCorsMappings(CorsRegistry registry) {
+	            registry.addMapping("/**").allowedOrigins("*");
+	         }
+	      };
+	   }
 
 }
